636dc698f7d78d317af12707ac900ffb121f6539
nada-intake40 <n.a.hassan1994@gmail.com>
2020-06-09 04:16:58 +0200

    make user update

---
+++ src/app/views/base/user-edit-form/user-edit-form.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { UsersService } from '../../../services/users.service';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute , Router} from '@angular/router';
+import { RolesService } from '../../../services/roles.service';
+
+
+
+@Component({
+  selector: 'app-user-edit-form',
+  templateUrl: './user-edit-form.component.html',
+  styleUrls: ['./user-edit-form.component.css']
+})
+export class UserEditFormComponent implements OnInit {
+  user;
+  users;
+  id:number;
+  roles;
+  
+  filedata:any;
+    fileEvent(e){      
+    this.filedata = e.target.files[0];
+    }
+
+  constructor(private _userservice:UsersService,
+              private route:ActivatedRoute ,
+              private _rolesService: RolesService,
+              private router:Router ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params =>{
+      this.id = +params['id'];
+    });
+
+    this._userservice.getUserById(this.id).subscribe(data =>{
+      this.user=data ;
+      console.log(data);
+    });
+
+    this._userservice.getUsers().subscribe((res:any) =>{
+      this.users=res.data;
+    });
+
+    this._rolesService.getRoles().subscribe(roleData =>{
+      this.roles=roleData;
+    });
+
+  }
+  
+  editUser(form: NgForm){
+    if(form.valid){
+      let myFormData= new FormData();
+       myFormData.append('avatar',this.filedata,this.filedata.name)
+       
+       Object.entries(form.value).map(value =>{
+         myFormData.append(`${value[0]}`,`${value[1]}`);
+       })
+
+       this._userservice.editUser(this.id,myFormData).subscribe((res: any)=>{
+         console.log(res);
+         
+       }
+       ,(error)=> {console.error(error);}
+       , () => {form.reset();}
+       );
+    }
+    this.router.navigate(['/base/Users']);
+  }
+}
--- src/app/views/base/users-form/users-form.component.ts
+++ src/app/views/base/users-form/users-form.component.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { UsersService } from '../../../services/users.service';
 import { NgForm } from '@angular/forms';
 import { RolesService } from '../../../services/roles.service';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users-form',
@@ -13,15 +13,15 @@ import { RolesService } from '../../../services/roles.service';
 export class UsersFormComponent implements OnInit {
   users;
   roles;
+  is_added= false;
   filedata:any;
     fileEvent(e){      
     this.filedata = e.target.files[0];
     }
-  constructor(private _userservice:UsersService,private _rolesService: RolesService) { }
+  constructor(private _userservice:UsersService,private _rolesService: RolesService,private route:Router) { }
 
   ngOnInit(): void {
     this._rolesService.getRoles().subscribe(roleData =>{
-      console.log(roleData);
       this.roles=roleData;
     });
 
@@ -37,13 +37,14 @@ export class UsersFormComponent implements OnInit {
         myFormData.append('avatar',this.filedata,this.filedata.name)
         
         Object.entries(form.value).map(value =>{
-          
           myFormData.append(`${value[0]}`,`${value[1]}`);
         })
       this._userservice.addUser(myFormData).subscribe((res: any) => {
         console.log(res);
+        this.is_added=true;
+        form.reset;
+        this.route.navigate(['/base/Users']);
       })
     }
-    form.reset;
   }
 }
