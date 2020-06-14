7c4f324d8fec82c2816b0921e345cfc50777b054
nada-intake40 <n.a.hassan1994@gmail.com>
2020-06-14 03:37:39 +0200

    editing criteria form

--- src/app/views/base/criteria-forms/criteria-forms.component.ts
+++ src/app/views/base/criteria-forms/criteria-forms.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute , Router} from '@angular/router';
 import { CriteriasService } from '../../../services/criterias.service';
 import { FireAlertService } from 'src/app/services/fire-alert.service';
 import { from } from 'rxjs';
-
+import { RolesService } from '../../../services/roles.service';
 
 @Component({
   selector: 'app-criteria-forms',
@@ -13,26 +13,34 @@ import { from } from 'rxjs';
 })
 export class CriteriaFormsComponent implements OnInit {
   types;
+  positions;
   constructor(private _criteriasService: CriteriasService,
     private route:ActivatedRoute ,
-    private router:Router ,private alert:FireAlertService) { }
+    private router:Router ,private alert:FireAlertService , 
+    private roleservice:RolesService) { }
 
   ngOnInit(): void {
     this._criteriasService.getCriteriaTypes().subscribe(dataType =>{      
       this.types = dataType;
-    })
+    });
+
+    this.roleservice.getRoles().subscribe(roles => {
+      this.positions=roles ;
+      console.log(roles);
+      
+    });
 
   }
 
   onSubmit(form: NgForm){
     if(form.valid){
       console.log(form.value);
-      this._criteriasService.addCriteria(form.value).subscribe((res: any)=>{
-        console.log(res);
-        if(res["errors"] && res["errors"]["name"]){
-          this.alert.fireAlert("error",res["errors"]["name"][0],"");
-        }
-    });
+    //   this._criteriasService.addCriteria(form.value).subscribe((res: any)=>{
+    //     console.log(res);
+    //     if(res["errors"] && res["errors"]["name"]){
+    //       this.alert.fireAlert("error",res["errors"]["name"][0],"");
+    //     }
+    // });
     form.reset();
     this.router.navigate(['/base/criteria'])
     }
