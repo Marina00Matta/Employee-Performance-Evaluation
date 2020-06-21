965291662aec0b669a89b748318937558cb041a1
nada-intake40 <n.a.hassan1994@gmail.com>
2020-06-21 19:19:42 +0200

    adding grps to user

--- src/app/views/base/users-form/users-form.component.ts
+++ src/app/views/base/users-form/users-form.component.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { UsersService } from '../../../services/users.service';
 import { NgForm } from '@angular/forms';
 import { RolesService } from '../../../services/roles.service';
+import { GroupService } from '../../../services/group.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -13,17 +14,23 @@ import { Router } from '@angular/router';
 export class UsersFormComponent implements OnInit {
   users;
   roles;
+  grps;
   is_added= false;
   filedata:any;
     fileEvent(e){      
     this.filedata = e.target.files[0];
     }
-  constructor(private _userservice:UsersService,private _rolesService: RolesService,private route:Router) { }
+  constructor(private _userservice:UsersService,private _rolesService: RolesService,
+              private route:Router , private _grpService:GroupService) { }
 
   ngOnInit(): void {
     this._rolesService.getRoles().subscribe(roleData =>{
       this.roles=roleData;
     });
+    
+    this._grpService.getGroups().subscribe(data =>{
+      this.grps = data ;
+    })
 
     this._userservice.getUsers().subscribe((res:any) =>{
       this.users=res.data;
@@ -38,13 +45,17 @@ export class UsersFormComponent implements OnInit {
         
         Object.entries(form.value).map(value =>{
           myFormData.append(`${value[0]}`,`${value[1]}`);
+          console.log(value[0], value[1] , typeof(value[1]));
+          
         })
-      this._userservice.addUser(myFormData).subscribe((res: any) => {
-        console.log(res);
-        this.is_added=true;
-        form.reset;
-        this.route.navigate(['/base/Users']);
-      })
+        // console.log(myFormData);
+        
+      // this._userservice.addUser(myFormData).subscribe((res: any) => {
+      //   console.log(res);
+      //   this.is_added=true;
+      //   form.reset;
+      //   this.route.navigate(['/base/Users']);
+      // })
     }
   }
 }
