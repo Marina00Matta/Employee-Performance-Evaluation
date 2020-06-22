adb603a4bc46972fc612f0b5101b7759849e533f
Marina00Matta <marinamedhat_19@hotmail.com>
2020-06-22 21:53:40 +0200

    create group through group form and user form

--- src/app/views/base/groups/groups.component.spec.ts
+++ src/app/views/base/groups/groups.component.spec.ts
@@ -22,4 +22,6 @@ describe('GroupsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  
 });
--- src/app/views/base/groups/groups.component.ts
+++ src/app/views/base/groups/groups.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GroupService } from '../../../services/group.service';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute , Router} from '@angular/router';
+
 @Component({
   selector: 'app-groups',
   templateUrl: './groups.component.html',
@@ -7,15 +10,25 @@ import { GroupService } from '../../../services/group.service';
 })
 export class GroupsComponent implements OnInit {
   groups;
-  constructor(private _groupService:GroupService) { }
+  constructor(private _groupService:GroupService,
+    private route:ActivatedRoute ,
+    private router:Router) { }
 
   ngOnInit(): void {
-     this.getAllGroup();
+     
   }
  
-  getAllGroup() {
-     this._groupService.getGroups().subscribe(data =>{
-       this.groups = data ;
-     });
-  }
+   onSubmit(form: NgForm){
+      if(form.valid){
+      console.log(form.value);
+      this._groupService.addGroup(form.value).subscribe((res: any)=>{
+        console.log(res);
+      });
+      form.reset();
+      this.router.navigate(['/base/Users']);
+      }
+    }
+
+  
+
 }
